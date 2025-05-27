cdadfbf9fb3366679957f64eae8ca63ec18118ad
S Anand <root.node@gmail.com>
2025-05-27 22:56:40 +0800

    WIP: PUML

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 A collection of tutorials that captures my learning journey.
 
+- 27 May 2025. [How to create a PlantUML architecture diagram with ChatGPT](plantuml/)
 - 26 Feb 2025. [How to set up an authenticating reverse proxy with OAuth2-Proxy and Caddy](oauth2-proxy/)
---
+++ plantuml/icons.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "' AWS icon macros. They all accept (e_alias, e_label, e_techn). Prefer including AWSPuml/[CATEGORY]/all.puml" > AWS.puml
+echo '!define AWSPuml https://raw.githubusercontent.com/awslabs/aws-icons-for-plantuml/refs/heads/main/dist' >> AWS.puml
+curl https://raw.githubusercontent.com/awslabs/aws-icons-for-plantuml/refs/heads/main/AWSSymbols.md \
+  | grep -Po '\b[\w./-]+\.puml\b' \
+  | sed 's/^/!includeurl AWSPuml\//' \
+  >> AWS.puml
+
+echo "' Azure icon macros. They all accept (e_alias, e_label, e_techn). Prefer including AzurePuml/[CATEGORY]/all.puml. Always include AzurePuml/AzureCommon.puml
+" > Azure.puml
+echo '!define AzurePuml https://raw.githubusercontent.com/plantuml-stdlib/Azure-PlantUML/master/dist' >> Azure.puml
+curl https://raw.githubusercontent.com/plantuml-stdlib/Azure-PlantUML/refs/heads/master/AzureSymbols.md \
+  | grep -Po '\b[\w./-]+\.puml\b' \
+  | grep -v 'AzurePuml/' \
+  | sed 's/^/!includeurl AzurePuml\//' \
+  >> Azure.puml
+
+echo "' GCP icon macros. They all accept (e_alias, e_label, e_techn). Prefer including GCPPuml/[CATEGORY]/all.puml. Always include GCPPuml/GCPCommon.puml
+" > GCP.puml
+echo '!define GCPPuml https://raw.githubusercontent.com/Crashedmind/PlantUML-icons-GCP/refs/heads/master/dist' >> GCP.puml
+curl https://raw.githubusercontent.com/Crashedmind/PlantUML-icons-GCP/refs/heads/master/Symbols.md \
+  | grep -Po '\b[\w./-]+\.puml\b' \
+  | sed 's/^/!includeurl GCPPuml\//' \
+  >> GCP.puml
